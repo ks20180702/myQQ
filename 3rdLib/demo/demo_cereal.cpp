b9a7d29a80b65108f215a026c7e1661464837450
ks <2671472814@qq.com>
2024-01-26 11:35:09 +0800

    测试cereal的使用，无误。

---
+++ 3rdLib/demo/demo_cereal.cpp
@@ -0,0 +1,113 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+#include <cereal/archives/json.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+
+struct Dat
+{
+	int x;
+	int y;
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(cereal::make_nvp("x", x), cereal::make_nvp("y", y));
+	}
+};
+
+
+struct Data
+{
+	Dat dat = { 2,3 };
+	int index = 1;
+	double d[3] = { 1.0,2.33e-5,1000000.0 };
+	std::vector<std::string> vs;
+	template <class Archive>
+	void serialize(Archive & ar)
+	{
+		ar(cereal::make_nvp("Dat", dat), cereal::make_nvp("index", index), cereal::make_nvp("d", d), cereal::make_nvp("vs", vs));
+	}
+	
+};
+
+int main()
+{
+    Data mydata;
+	mydata.dat.x = 50;
+	mydata.dat.y = 150;
+	std::string s1 = "中文字符串/*-+!@#$%^&";
+	std::string s2 = "english/*-+!@#$%^&";
+	mydata.vs.push_back(s1);
+	mydata.vs.push_back(s2);
+	
+
+	// std::ofstream file("out.json");
+    // std::string pp;
+    std::ostringstream ss;
+    
+	cereal::JSONOutputArchive archive(ss);
+	archive(cereal::make_nvp("mydata", mydata));
+    std::cout<<ss.str()<<std::endl;
+
+
+}
+
+// ------------------------------------------
+// struct Data
+// {
+// 	int index = 1;
+// 	double d[3] = { 1.0,2.33e-5,1000000.0 };
+// 	std::vector<std::string> vs;
+
+// 	template <class Archive>
+// 	void serialize(Archive & ar)
+// 	{
+//         // ar(index, d, vs);
+// 		ar(cereal::make_nvp("index", index), cereal::make_nvp("d", d), cereal::make_nvp("vs", vs));
+// 	}
+// };
+
+// int main()
+// {
+
+// 	Data mydata;
+// 	std::string s1 = "中文字符串/*-+!@#$%^&";
+// 	std::string s2 = "english/*-+!@#$%^&";
+// 	mydata.vs.push_back(s1);
+// 	mydata.vs.push_back(s2);
+// 	std::ofstream file("out1.json");
+
+// 	cereal::JSONOutputArchive archive(file);
+//     // archive(CEREAL_NVP(mydata.index), CEREAL_NVP(mydata.d), CEREAL_NVP(mydata.vs));
+// 	archive(CEREAL_NVP(mydata));
+
+// 	// std::ifstream file("out1.json");
+// 	// cereal::JSONInputArchive archive(file);
+// 	// int n;
+// 	// std::vector<std::string> s;
+// 	// double d[3];
+//     // Data mydata;
+// 	// // archive(cereal::make_nvp("mydata.index", n));
+// 	// // archive(cereal::make_nvp("mydata.d", d));
+// 	// // archive(cereal::make_nvp("mydata.vs", s));
+//     // archive(cereal::make_nvp("mydata", mydata));
+// 	// std::cout <<mydata.index << '\t'  << '\t' << mydata.d[1]; 
+
+//     return 0;
+// }
+
+// ----------------------------------------------
+    // std::ofstream file("out.json");
+	// cereal::JSONOutputArchive archive(file);
+	// std::string s[] = { "this is a string", " 中文string也是可以支持的" };
+	// std::vector<double> vec = { 1.00001, 2e3, 30.1, -4, 5 };
+	// archive(CEREAL_NVP(vec), CEREAL_NVP(s));
+
+    // std::ifstream file("out.json");
+	// cereal::JSONInputArchive archive(file);
+	// std::string s[2];
+	// archive(CEREAL_NVP(s));
+	// std::cout << s[1];
